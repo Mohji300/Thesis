2e054857e927d4045aa8b287fecd493a05076d9f
Mohji300 <ramirezjhncrl@gmail.com>
2025-02-28 08:31:02 +0800

    added mock data for systems checking

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContentComponent } from './content/content.component';
 import { SummaryComponent } from './summary/summary.component';
 
 export const routes: Routes = [
-    { path : 'home', component: HomeComponent },
-    { path : 'content', component: ContentComponent },
-    { path : 'summary', component: SummaryComponent },
-    { path : '', redirectTo: '/home', pathMatch: 'full' }
+    { path: 'home', component: HomeComponent },
+    { path: 'content', component: ContentComponent },
+    { path: 'summary', component: SummaryComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
